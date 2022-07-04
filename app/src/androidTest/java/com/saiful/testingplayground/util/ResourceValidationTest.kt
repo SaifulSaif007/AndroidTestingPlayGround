2e5f7a114a8ade986f91b26e48d99e3c4f7bfcd7
Saiful Islam <saifulsaif321@gmail.com>
2022-07-05 00:07:27 +0600

    test : android component with context

---
+++ app/src/androidTest/java/com/saiful/testingplayground/util/ResourceValidationTest.kt
@@ -0,0 +1,39 @@
+package com.saiful.testingplayground.util
+
+import android.content.Context
+import androidx.test.core.app.ApplicationProvider
+import com.google.common.truth.Truth.assertThat
+import org.junit.Test
+import com.saiful.testingplayground.R
+import org.junit.After
+import org.junit.Before
+
+class ResourceValidationTest {
+
+    private lateinit var resourceValidation: ResourceValidation
+
+    @Before
+    fun setup() {
+        resourceValidation = ResourceValidation()
+    }
+
+    @After
+    fun tearDown(){
+        // Anything to execute after each test run
+    }
+
+    @Test
+    fun stringSameAsResource_ReturnsTrue() {
+        val context = ApplicationProvider.getApplicationContext<Context>()
+        val result =
+            resourceValidation.isResourceEqual(context, R.string.app_name, "TestingPlayGround")
+        assertThat(result).isTrue()
+    }
+
+    @Test
+    fun stringDifferentAsResource_ReturnsFalse() {
+        val context = ApplicationProvider.getApplicationContext<Context>()
+        val result = resourceValidation.isResourceEqual(context, R.string.app_name, "Tsdlhsahdk")
+        assertThat(result).isFalse()
+    }
+}
